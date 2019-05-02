77f3e77e20216d9665cd7b2322c807c39e0c0266
Molly Stone <molly.stone89@gmail.com>
2019-05-02 18:02:02 -0400

    Create new entry view page, new entry route working

--- controllers/ally.js
+++ controllers/ally.js
@@ -2,14 +2,29 @@ const express = require('express');
 const ally = express.Router();
 const Entry = require('../models/entries.js');
 
+/****************
+POST
+****************/
+
+ally.post('/', (req, res) => {
+    res.send('route working');
+});
+
+/****************
+GET
+****************/
+
+// INDEX ROUTE
 ally.get('/', (req, res) => {
     res.render('ally/index.ejs');
 });
 
+// NEW ENTRY ROUTE
 ally.get('/new-entry', (req, res) => {
-    res.send('route working');
+    res.render('ally/new-entry.ejs');
 });
 
+// SHOW ENTRIES ROUTE
 ally.get('/entries', (req, res) => {
     res.send('route working');
 });
