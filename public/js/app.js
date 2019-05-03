788132ecb5d570e55789b792b6f124c9c0397cb9
Molly Stone <molly.stone89@gmail.com>
2019-05-03 18:17:31 -0400

    Now able to add goals and complete goals on index page

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+// $(() => {
+//     $('#complete').on('click', (event) => {
+//         $('#goal-header').text('set a new goal');
+//         console.log($(this).parent());
+//         $('.goal').append('<a href="/ally/new-goal"><button type="button" name="new-goal">New Goal</button></a>')
+//     });
+// });
